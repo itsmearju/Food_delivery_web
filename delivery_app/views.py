5610a13c7ab38ccf0ee6365bc345a42c9b3c0c85
Arjun Shaji <arjunshaji54@gmail.com>
2023-06-02 23:49:13 +0530

    admin & fn for getting all item

--- delivery_app/views.py
+++ delivery_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from .models import MenuItem, Category, OrderModel
 
 # Create your views here.
 
@@ -10,3 +11,18 @@ class Index(View):
 class About(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'customer/about.html')
+
+class Order(View):
+    def get(self, request, *args, **kwargs):
+        
+        # get every item from each category
+        appetizers = MenuItem.objects.filter(category_name_contains='Appetizer')
+        entres = MenuItem.objects.filter(category_name_contains='Entre')
+        desserts = MenuItem.objects.filter(category_name_contains='Dessert')
+        drinks = MenuItem.objects.filter(category_name_contains='Drink')
+
+        # pass into context
+        context = { 'appetizers':appetizers, 'entres':entres, 'desserts':desserts, 'drinks':drinks }
+
+        # render the template
+        return render(request, 'customer/order.html', context)

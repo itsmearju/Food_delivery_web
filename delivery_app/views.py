118e26788e795c90744b4304a7ab4554ca1a94d2
Arjun Shaji <arjunshaji54@gmail.com>
2023-06-11 22:22:33 +0530

    order & order summary

--- delivery_app/views.py
+++ delivery_app/views.py
@@ -47,18 +47,18 @@ class Order(View):
             price = 0
             item_ids = []
 
-            for item in order_items['items']:
-                price += item['price']
-                item_ids.append(item['id'])
+        for item in order_items['items']:
+            price += item['price']
+            item_ids.append(item['id'])
 
-            order = OrderModel.objects.create(price=price)
-            order.items.add(*item_ids)
+        order = OrderModel.objects.create(price=price)
+        order.items.add(*item_ids)
 
-            context = {
-                'items': order_items['items'],
-                'price': price
-            }
+        context = {
+            'items': order_items['items'],
+            'price': price
+                }
 
-            return render(request, 'customer/order_confirmation.html', context)
+        return render(request, 'customer/order_confirmation.html', context)
 
 
--- delivery_project/urls.py
+++ delivery_project/urls.py
@@ -18,10 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from delivery_app.views import Index, About
+from delivery_app.views import Index, About, Order
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Index.as_view(), name='index'),
-    path('about/', About.as_view(), name='about')
+    path('about/', About.as_view(), name='about'),
+    path('order/',Order.as_view(), name='order'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
